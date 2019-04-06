864c3f2bb14f65166315a837404e360f88359887
Milan Koniar <milankoniar@gmail.com>
2019-04-06 20:46:38 +0200

    tuples

--- index.ts
+++ index.ts
@@ -1,7 +1,1 @@
-const example1: string[] = ['Hello World'];
-
-const example2: (number | boolean)[] = [1, 2, true];
-
-const example3: boolean[] = [true, false];
-
-const example4: boolean[][] = [ [true, false], [true, true] ] ;
+const exampleTuple: [string, number] = ['https://www.YouTube.com/CodingTutorials360', 30 ];
