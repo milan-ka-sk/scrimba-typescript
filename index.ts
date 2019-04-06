ff8e31bd69ec5f476a86c335027fdf9b411e87ff
Milan Koniar <milankoniar@gmail.com>
2019-04-06 21:24:35 +0200

    parameters

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-const exampleTuple: [string, number] = ['https://www.YouTube.com/CodingTutorials360', 30 ];
+import { Person } from './person.model';
+
+function add(val1: number, val2: number) {
+    return val1 + val2;
+}
+
+add(1, 10);
+
+function sayHello(person: Person) {
+    return `Say Hello to My Little Friend, ${person.firstName}!`    
+}
+
+sayHello(new Person({firstName: 'Dylan'}));
