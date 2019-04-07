2e3085e6bd9e980898886081ab4fa16dc7d78dd8
Milan Koniar <milankoniar@gmail.com>
2019-04-07 09:36:13 +0200

    intersection types

--- person.model.ts
+++ person.model.ts
@@ -1,11 +1,17 @@
-export class Person {
-    firstName: string;
-    middleName: string;
-    lastName: string;
-    
-    constructor(data?: any) {
-        this.firstName = data.firstName || 'Dylan';
-        this.lastName = data.lastName || 'Israel';
-        this.middleName = data.middleName;    
-    }
-}
+import { Bear, Man, Pig } from './interfaces';
+
+let man: Man | Pig;
+let bear: Bear;
+let pig: Pig;
+
+
+// var 1
+// let manBearPig: Bear & Man & Pig;
+
+// var 2
+type ManBearPig = Bear & Man & Pig
+let manBearPig: ManBearPig ;
+
+manBearPig.firstName = 'Dylan';
+manBearPig.claws = 3;
+manBearPig.bacon = false;
